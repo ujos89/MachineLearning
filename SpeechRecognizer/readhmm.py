f8fbb523a578825057df40daca59625446bbcd93
ujos89 <48623147+ujos89@users.noreply.github.com>
2020-12-11 03:36:57 +0900

    Add files via upload

--- SpeechRecognizer/readhmm.py
+++ SpeechRecognizer/readhmm.py
@@ -6,7 +6,7 @@ phone_list = []
 phone = []
 
 # initalize signals for text mining 
-mu_sig = sigma_sig = pdf_sig = state_sig = transp_sig = 0
+hmm_sig, mu_sig, sigma_sig, pdf_sig, state_sig, transp_sig = 0,0,0,0,0,0
 transp = []
 pdf = []
 state = []
@@ -18,49 +18,53 @@ while True:
     line_tmp = line[:].strip()
 
     if line_tmp.startswith('~h'):
-        name = line_tmp.replace('~h','').replace('"','')
+        hmm_sig = 1
+        name = line_tmp.replace('~h ','').replace('"','')
         phone_list.append(name)
         phone_sig = 1
-    elif line_tmp.startswith('<NUMSTATES>'):
-        state_sig = int(line_tmp[-1]) - 1
-    elif line_tmp.startswith('<STATE>'):
-        state_sig -= 1
-    elif line_tmp.startswith('<NUMMIXES>'):
-        pdf_sig = int(line_tmp[-1]) + 1
-    elif line_tmp.startswith('<MIXTURE>'):
-        w = np.array(float(line_tmp.split()[-1]))
-        pdf.append(w)
-    elif line_tmp.startswith('<MEAN>'):
-        mu_sig = 1
-    elif line_tmp.startswith('<VARIANCE>'):
-        sigma_sig = 1
-    elif line_tmp.startswith('<TRANSP>'):
-        transp_sig = int(line_tmp[-1])
-    elif line_tmp.startswith('<ENDHMM>'):
-        transp = np.array(transp)
-        hmm.append(transp)
-        transp = []
-        phone.append(hmm)
-        hmm = [] 
 
-    if(mu_sig and (not line_tmp.startswith('<MEAN>'))):
-        mu_sig -= 1
-        mu = np.array([float(x) for x in line_tmp.split()])
-        pdf.append(mu)
-    if(sigma_sig and (not line_tmp.startswith('<VARIANCE>'))):
-        sigma_sig -= 1
-        sigma = np.array([float(x) for x in line_tmp.split()])
-        pdf.append(sigma)
-        pdf_sig -= 1
-        state.append(pdf)
-        pdf = []
-    if(pdf_sig == 1):
-        pdf_sig -= 1
-        hmm.append(state)
-        state = []
-    if(transp_sig and (not line_tmp.startswith('<TRANSP>'))):
-        transp_sig -= 1
-        transp_tmp = [float(x) for x in line_tmp.split()]
-        transp.append(transp_tmp)
+    if (hmm_sig == 1):
+        if line_tmp.startswith('<NUMSTATES>'):
+            state_sig = int(line_tmp[-1]) - 1
+        elif line_tmp.startswith('<STATE>'):
+            state_sig -= 1
+        elif line_tmp.startswith('<NUMMIXES>'):
+            pdf_sig = int(line_tmp[-1]) + 1
+        elif line_tmp.startswith('<MIXTURE>'):
+            w = np.array(float(line_tmp.split()[-1]))
+            pdf.append(w)
+        elif line_tmp.startswith('<MEAN>'):
+            mu_sig = 1
+        elif line_tmp.startswith('<VARIANCE>'):
+            sigma_sig = 1
+        elif line_tmp.startswith('<TRANSP>'):
+            transp_sig = int(line_tmp[-1])
+        elif line_tmp.startswith('<ENDHMM>'):
+            hmm_sig = 0
+            transp = np.array(transp)
+            hmm.append(transp)
+            transp = []
+            phone.append(hmm)
+            hmm = [] 
+
+        if(mu_sig and (not line_tmp.startswith('<MEAN>'))):
+            mu_sig -= 1
+            mu = np.array([float(x) for x in line_tmp.split()])
+            pdf.append(mu)
+        if(sigma_sig and (not line_tmp.startswith('<VARIANCE>'))):
+            sigma_sig -= 1
+            sigma = np.array([float(x) for x in line_tmp.split()])
+            pdf.append(sigma)
+            pdf_sig -= 1
+            state.append(pdf)
+            pdf = []
+        if(pdf_sig == 1):
+            pdf_sig -= 1
+            hmm.append(state)
+            state = []
+        if(transp_sig and (not line_tmp.startswith('<TRANSP>'))):
+            transp_sig -= 1
+            transp_tmp = [float(x) for x in line_tmp.split()]
+            transp.append(transp_tmp)
 
 f.close()
--- SpeechRecognizer/utterance.py
+++ SpeechRecognizer/utterance.py
@@ -1,4 +1,64 @@
+import numpy as np
 from readhmm import phone, phone_list
 from readtrn import trn
 
-print(trn[1][1])
+def build_utterancehmm(trn_utt, phone_list, phone):
+    #ready for transp_seq, state_seq
+    state_seq = []
+    transp_seq = []
+    for phone_hmm in trn_utt[1]:
+        tmp_idx = phone_list.index(phone_hmm)
+        transp_seq.append(tmp_idx)
+        '''
+        if(tmp_idx != 17):
+            state_seq.append(tmp_idx)
+            state_seq.append(tmp_idx)
+            state_seq.append(tmp_idx)
+        else:
+            state_seq.append(tmp_idx)
+        '''
+    #state_seq = np.array(state_seq)
+    transp_seq = np.array(transp_seq)
+    #print(state_seq, type(state_seq), len(state_seq))
+    #print(transp_seq, type(transp_seq), len(transp_seq))
+ 
+    ## build theta
+    #build transition probabilty
+    before_transp_phone = -1
+    for transp_phone in transp_seq:
+        if(before_transp_phone != -1):
+            transp_now = phone[transp_phone][-1]
+            transp_len = len(transp)
+            # + "sp"
+            if(transp_phone == 17):
+                transp_tmp = np.zeros((transp_len+1,transp_len+1))
+                transp_tmp[:-1,:-1] = transp
+                transp_tmp[-2,-2:] = transp_now[1,1:2]
+                transp_tmp[-3,-2:] = transp_tmp[-3,-2] * transp_now[0,1:2]
+                transp = transp_tmp
+            # + other phone
+            else:
+                transp_tmp = np.zeros((transp_len+3,transp_len+3))
+                transp_tmp[0:transp_len-1, 0:transp_len] = transp[0:transp_len-1, :]
+                transp_tmp[-4:,-4:] = transp_now[-4:,-4:]
+                transp = transp_tmp
+        else:
+            transp = phone[transp_phone][-1]
+        before_transp_phone = transp_phone
+    #print(transp, type(transp), len(transp))
+
+    #build weight, mu, sigma
+    states = []
+    for idx in transp_seq:
+        if(idx == 17):
+            states.append(phone[idx][0])
+        else:
+            states.append(phone[idx][0])
+            states.append(phone[idx][1])
+            states.append(phone[idx][2])
+    #state => staes[staes][pdf][w, mu, sigma]
+
+    return transp_seq, transp, states
+
+#how to use
+phone_seq, transp, theta = build_utterancehmm(trn[0], phone_list, phone)
